a7031fe874227f998954db49f9ee250b49ab919e
Jose Pablo Flores <josepablofr@gmail.com>
2023-09-26 21:59:05 -0600

    Included Safeargs and Navigation in horoscope Activity

---
+++ app/src/main/java/com/example/horoscapp/domain/model/HoroscopeModel.kt
@@ -0,0 +1,5 @@
+package com.example.horoscapp.domain.model
+
+enum class HoroscopeModel {
+    Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces
+} 
