058141aeef79191710c5cf8f667e89412f1d8a66
Jose Pablo Flores <josepablofr@gmail.com>
2023-09-28 23:49:32 -0600

    Repostitories Configuration

--- app/src/main/java/com/example/horoscapp/data/RepositoryImpl.kt
+++ app/src/main/java/com/example/horoscapp/data/RepositoryImpl.kt
@@ -1,12 +1,18 @@
 package com.example.horoscapp.data
 
+import android.util.Log
 import com.example.horoscapp.data.network.HoroscopeApiService
+import com.example.horoscapp.data.network.response.PredictionResponse
 import com.example.horoscapp.domain.Repository
-import retrofit2.Retrofit
+import com.example.horoscapp.domain.model.PredictionModel
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(private val apiService: HoroscopeApiService): Repository {
-    override suspend fun getPrediction(sign: String) {
 
+    override suspend fun getPrediction(sign: String): PredictionModel? {
+        runCatching { apiService.getHoroscope(sign) }
+            .onSuccess { return it.toDomain() }
+            .onFailure { Log.i("pepe", "Ha ocurrido un error ${it.message}")  }
+        return null
     }
 }
--- app/src/main/java/com/example/horoscapp/domain/Repository.kt
+++ app/src/main/java/com/example/horoscapp/domain/Repository.kt
@@ -1,5 +1,7 @@
 package com.example.horoscapp.domain
 
+import com.example.horoscapp.domain.model.PredictionModel
+
 interface Repository {
-    suspend fun getPrediction(sign:String)
+    suspend fun getPrediction(sign:String): PredictionModel?
 }
