2d0fe0e3f3def88aa56f961ff014bcf4ec538855
Jose Pablo Flores <josepablofr@gmail.com>
2023-10-04 21:04:54 -0600

    tests configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ android {
 
 dependencies {
 
+    testImplementation("junit:junit:4.12")
     val navVersion = "2.7.1"
     val cameraVersion = "1.2.3"
 
@@ -85,7 +86,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //UnitTesting
     testImplementation("junit:junit:4.13.2")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
+
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
---
+++ app/src/test/java/com/example/horoscapp/data/network/response/PredictionResponseTest.kt
@@ -0,0 +1,16 @@
+package com.example.horoscapp.data.network.response
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class PredictionResponseTest {
+
+    @Test
+    fun `toDomain should return a correct PredictionModel`(){
+        //Given
+
+        //When
+
+        //Then
+    }
+}
